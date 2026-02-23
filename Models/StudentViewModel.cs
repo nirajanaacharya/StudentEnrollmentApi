64d4a1d366296241fb85189eeb3477373f5b0dc4
nirajanaacharya <nirajanlegendkilker15@gmail.com>
2026-02-23 17:39:48 +0545

    Added some of the css

--- Models/StudentViewModel.cs
+++ Models/StudentViewModel.cs
@@ -4,15 +4,15 @@ namespace StudentEnrollmentApi.Models
 {
     public class StudentViewModel
     {
+        
         public List<Student> Students { get; set; } = new List<Student>();
         
-       
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = string.Empty;
-        
-      
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; } = string.Empty;
-        
-      
+    
+        [Required(ErrorMessage = "Department is required")]
         public string Department { get; set; } = string.Empty;
     }
 }
